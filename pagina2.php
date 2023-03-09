10eb0e0cbafcd3e8fc6446f8df5145d9b6ea188c
JUAN VITOR TASCA DE ANDRADE <juan.4910@aluno.pr.senac.br>
2023-03-09 11:57:48 -0300

    atualuzaan

---
+++ pagina2.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Atividade</title>
+    </head>
+    <body>
+        <?php
+            $valor = 6;
+            $valor2 = 3;
+            $valor3 = 45;
+            $valor4 = 80;
+
+            if ($valor > $valor2)
+            echo "$valor é maior que $valor2";
+            else 
+            echo "$valor é menor que $valor2";
+            echo ("</br>");
+            if ($valor3 < $valor4)
+            echo "$valor3 é menor que $valor4";
+            else 
+            echo "$valor3 é maior que $valor4";
+            
+            
+        ?>
+    </body>
+</html>
